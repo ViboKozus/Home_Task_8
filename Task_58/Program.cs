413554a8f7843f2b3f7038f0cee85145245c322b
Georgii Bondarenko <bgv900@mail.ru>
2023-04-02 16:59:45 +0300

    Задача 58

--- Task_58/Program.cs
+++ Task_58/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); 
+Console.WriteLine("Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц."); 
+ 
+Console.Write("Задайте размер матрицы: "); 
+int N; 
+int.TryParse(Console.ReadLine()!, out N); 
+int[,] FtMartrix = new int[N, N];
+int[,] SecMartrix = new int[N, N]; 
+Console.WriteLine("1-я Матрица: ");
+for (int i = 0; i < N; i++) 
+{ 
+    for (int j = 0; j < N; j++) 
+    { 
+        FtMartrix[i, j] = new Random().Next(1, 10); 
+        Console.Write($"{FtMartrix[i, j], -2} "); 
+    } 
+    Console.WriteLine(); 
+} 
+
+Console.WriteLine("2-я Матрица: ");
+for (int i = 0; i < N; i++) 
+{ 
+    for (int j = 0; j < N; j++) 
+    { 
+        SecMartrix[i, j] = new Random().Next(1, 10); 
+        Console.Write($"{SecMartrix[i, j], -2} "); 
+    } 
+    Console.WriteLine(); 
+} 
+
+int[,] ResultMartrix = new int[N, N];
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            ResultMartrix[i, j] = ResultMartrix[i, j] + (FtMartrix[i, k] * SecMartrix[k, j]);
+        }
+    }
+}
+Console.WriteLine("Результат произведение двух матриц:   ");
+for (int i = 0; i < ResultMartrix.GetLength(0); i++)
+{
+    for (int j = 0; j < ResultMartrix.GetLength(1); j++)
+    {
+        Console.Write($"{ResultMartrix[i, j], -2} ");
+    }
+    Console.WriteLine(); 
+}
