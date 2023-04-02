6fb3bf5f9ddfe810d2e87a154b01ff06bd83ed19
Georgii Bondarenko <bgv900@mail.ru>
2023-04-02 12:41:33 +0300

    Задача 56 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,47 @@
+﻿#pragma warning disable 
+Console.Clear(); 
+Console.WriteLine("Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива."); 
+ 
+Console.Write("Введите кол-во строк: "); 
+int n; 
+int.TryParse(Console.ReadLine()!, out n); 
+Console.Write("Введите кол-во столбцов: "); 
+int m; 
+int.TryParse(Console.ReadLine()!, out m); 
+ 
+int[,] arr = new int[n, m]; 
+
+for (int i = 0; i < n; i++) 
+{ 
+    for (int j = 0; j < m; j++) 
+    { 
+        arr[i, j] = new Random().Next(1, 10); 
+        Console.Write($"{arr[i, j], -2} "); 
+    } 
+    Console.WriteLine(); 
+} 
+
+for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    Console.WriteLine(); }
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write($"{arr[i, j], -2} ");
+    }
+    Console.WriteLine(); 
+}
---
+++ Task_56/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear(); 
+Console.WriteLine("Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов."); 
+ 
+Console.Write("Введите кол-во строк: "); 
+int n; 
+int.TryParse(Console.ReadLine()!, out n); 
+Console.Write("Введите кол-во столбцов: "); 
+int m; 
+int.TryParse(Console.ReadLine()!, out m); 
+ 
+int[,] arr = new int[n, m]; 
+
+for (int i = 0; i < n; i++) 
+{ 
+    for (int j = 0; j < m; j++) 
+    { 
+        arr[i, j] = new Random().Next(1, 10); 
+        Console.Write($"{arr[i, j], -2} "); 
+    } 
+    Console.WriteLine(); 
+} 
+
+int MinSumLine = 0;
+int SumLine = SumLineNumbers(arr, 0);
+for (int i = 1; i < arr.GetLength(0); i++)
+{
+  int tempSumLine = SumLineNumbers(arr, i);
+  if (SumLine > tempSumLine)
+  {
+    SumLine = tempSumLine;
+    MinSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{MinSumLine+1} - номер строки с минимальной суммой ({SumLine}) элементов ");
+
+int SumLineNumbers(int[,] arr, int i)
+{
+  int sumLine = arr[i,0];
+  for (int j = 1; j < arr.GetLength(1); j++)
+  {
+    sumLine += arr[i,j];
+  }
+  return sumLine;
+}
+
