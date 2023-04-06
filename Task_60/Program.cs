bacda5d34b61db1ab171216c8ec1ed9195bef979
Georgii Bondarenko <bgv900@mail.ru>
2023-04-06 19:55:02 +0300

    Задача ДЗ

---
+++ Task_60/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear(); 
+Console.WriteLine("Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента."); 
+ 
+int[,,] arr = new int[2, 2, 2]; 
+List <int> num = new List<int> ();
+for (int i = 10; i < 100; i++)
+    num.Add(i);
+
+for (int i = 0; i < arr.GetLength(0)&& num.Count > 0; i++)
+{
+    for (int j = 0; j < arr.GetLength(1)&& num.Count > 0; j++)
+    {
+        for (int k = 0; k < arr.GetLength(2)&& num.Count > 0; k++)
+        {
+            int rant = new Random().Next(0, num.Count);
+            arr[i, j, k] = num[rant];
+            num.RemoveAt(rant);
+        }
+    }
+}
+//foreach (var item in arr)
+    //Console.Write($"{item} ");
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int k = 0; k < arr.GetLength(2); k++)
+        {
+            Console.Write($"{arr[i, j, k],2} ({i}, {j}, {k}) ");
+        }
+    }
+    Console.WriteLine();
+} 
---
+++ Task_62/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear(); 
+Console.WriteLine("Задача 62: Напишите программу, которая заполнит спирально массив 4 на 4."); 
+
+int N = 4;
+int M = 4;
+int[,] array = new int[N, M];
+
+int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
+int k = 1;
+int i = 0;
+int j = 0;
+
+while (k <= N * M)
+{
+    array[i, j] = k;
+    if (i == Ibeg && j < M - Jfin - 1)
+        ++j;
+    else if (j == M - Jfin - 1 && i < N - Ifin - 1)
+        ++i;
+    else if (i == N - Ifin - 1 && j > Jbeg)
+        --j;
+    else
+        --i;
+
+    if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != M - Jfin - 1))
+    {
+        ++Ibeg;
+        ++Ifin;
+        ++Jbeg;
+        ++Jfin;
+    }
+    ++k;
+}
+Console.WriteLine();
+for (int row = 0; row < array.GetLength(0); row++)
+{
+    for (int col = 0; col < array.GetLength(1); col++)
+    {
+        Console.Write($"{array[row, col],3}");
+    }
+    Console.WriteLine();
+}
